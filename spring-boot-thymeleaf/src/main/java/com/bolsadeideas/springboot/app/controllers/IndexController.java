e69b55191bd9a8aeb7ca2eeafb78574c6e06416a
jrobles-bilbomatica <jrobles@bilbomatica.es>
2019-02-20 16:55:26 +0100

    Ejemplo 2: spring-boot-thymeleaf

--- spring-boot-thymeleaf/src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
+++ spring-boot-thymeleaf/src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
+	@Value("${application.controllers.mensaje}")
+	private String mensaje;
+	
 	@GetMapping("/")
 	public String hola(Model model) {
-		model.addAttribute("mensaje", "Hola que tal! Spring Boot!");
+		model.addAttribute("mensaje", mensaje);
 		return "hola";
 	}
 
